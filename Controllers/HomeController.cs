9900db6b83c5c682ed78a064a39de0e56fd2fc9f
Haridas <haridasnykiel@gmail.com>
2019-02-14 22:29:58 +0000

    added a model.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,14 @@
+using HariFood.Models;
+using Microsoft.AspNetCore.Mvc;
+
 namespace HariFood.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
-        public string Index() {
-            return "Hello from home controller..........";
+        public IActionResult Index() {
+            var controllerName = this.ControllerContext.ActionDescriptor.ControllerName; // Get the controller name.
+            var model = new Resturant { Id = 1, Name = "Hari's Dirty Burgers" };
+            return new ObjectResult(model);
         }
     }
 }
---
+++ Models/Resturant.cs
@@ -0,0 +1,8 @@
+namespace HariFood.Models
+{
+    public class Resturant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
