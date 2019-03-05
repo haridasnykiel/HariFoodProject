d525636a101d86751541fc40de6674a12f5d3160
Haridas Nykiel <Haridas.Nykiel@jet2.com>
2019-03-05 13:38:12 +0000

    added a directive to html file.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace HariFood.Controllers {
     public class HomeController : Controller {
         public IActionResult Index () {
-            var controllerName = this.ControllerContext.ActionDescriptor.ControllerName; // Get the controller name.
+            //var controllerName = this.ControllerContext.ActionDescriptor.ControllerName; // Get the controller name.
             var model = new Resturant { Id = 1, Name = "Hari's Dirty Burgers" };
             //new ObjectResult(model);
             return View (model);
