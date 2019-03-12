53067e242e77ba67548b2d40f80454df4502b1e6
Haridas Nykiel <Haridas.Nykiel@jet2.com>
2019-03-12 13:56:10 +0000

    Added to the html file which will now display all the restaurants in the InMemoryRestaurantData object.

--- Services/InMemoryRestaurantData.cs
+++ Services/InMemoryRestaurantData.cs
@@ -4,10 +4,16 @@ using HariFood.Models;
 
 namespace HariFood.Services {
     public class InMemoryRestaurantData : IRestaurantData {
+
+        List<Restaurant> _restaurants;
+
         public InMemoryRestaurantData () {
             _restaurants = new List<Restaurant> {
-                new Restaurant { Id = 1, Name = "Haridas Chevda" },
                 new Restaurant { Id = 2, Name = "Kathryns Samosas" },
+                new Restaurant { Id = 1, Name = "Haridas Chevda" },
+                new Restaurant { Id = 1, Name = "Zulu" },
+                new Restaurant { Id = 1, Name = "Walk" },
+                new Restaurant { Id = 1, Name = "Yo" },
                 new Restaurant { Id = 3, Name = "Prashad" }
             };
         }
@@ -16,6 +22,6 @@ namespace HariFood.Services {
             return _restaurants.OrderBy (r => r.Name);
         }
 
-        List<Restaurant> _restaurants;
+        
     }
 }
